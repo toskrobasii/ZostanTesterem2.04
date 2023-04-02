303a900a3608b1a6e7afdbdf6a64cba4106088e9
Tomasz Oskroba <toskroba@sii.pl>
2023-04-02 15:15:41 +0200

    New test added

---
+++ src/test/java/PageSetup.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// dziedziczenie w Java
+public class PageSetup {
+
+    String projectLocation = System.getProperty("user.dir");
+    ChromeDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", projectLocation + "/resources/chromedriver.exe");
+
+        // biblioteka WebDriverManager
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+    }
+
+    @AfterEach
+    public void teardown() {
+        // różnica pomiędzy driver.close() a driver.quit()
+//         Ctrl + /
+        driver.quit();
+    }
+}
---
+++ src/test/java/UltimateQATest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+// modyfikatory dostępu: public, private, protected w Java - hermetyzacja w Java (enkapsulacja)
+public class UltimateQATest extends PageSetup {
+
+
+    // zwracanie z funkcji: jakis typ, void - nic nie zwraca
+    @Test
+    public void testOne() {
+        WebElement buttonUsingId = driver.findElement(By.id("idExample"));
+        buttonUsingId.click();
+
+//        driver.findElement(By.id("idExample")).click(); - rownowazne liniom 12-13
+
+        // Jbehave - notacja Gherkin: Given, When, Then, kryteria akceptacji
+        WebElement buttonSuccessText = driver.findElement(By.className("entry-title"));
+        Assertions.assertEquals("Button success", buttonSuccessText.getText());
+    }
+
+}
