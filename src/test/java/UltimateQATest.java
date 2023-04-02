6073f04cf14e90a56a5272d501dfb390cfda2bd8
Tomasz Oskroba <toskroba@sii.pl>
2023-04-02 15:55:55 +0200

    New test added

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -20,4 +20,28 @@ public class UltimateQATest extends PageSetup {
         Assertions.assertEquals("Button success", buttonSuccessText.getText());
     }
 
+    // dopisac przypadki testowe na button using ClassName i Name
+
+    @Test
+    public void testTwo() throws InterruptedException {
+        // sposoby na wyszukiwanie elementów przez Selenium: xpath, className, id, selektory CSS
+        WebElement nameInput = driver.findElement(By.id("et_pb_contact_name_0"));
+        nameInput.sendKeys("Tester");
+
+        WebElement emailInput = driver.findElement(By.id("et_pb_contact_email_0"));
+        emailInput.sendKeys("tester@tester.pl");
+
+        Thread.sleep(2000);
+
+        // waity w Selenium: implicit wait, explicit wait, fluent wait i Thread.sleep(2000)
+        WebElement emailMeButton = driver.findElement(By.name("et_builder_submit_button"));
+        emailMeButton.click();
+
+        Thread.sleep(2000);
+        // ogólna postać xpath:     //*[]
+        // odbijanie się od elementów: //*[@class='']/p     / - jeden poziom    // - 1 poziom i więcej
+        WebElement thanksText = driver.findElement(By.xpath("//div[@class='et-pb-contact-message']/p"));
+        Assertions.assertEquals("Thanks for contacting us", thanksText.getText());
+    }
+
 }
